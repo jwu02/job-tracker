2b49222f58e08571c62cec8a8f663b025d5a7612
jwu02 <jiacongwu31@gmail.com>
2024-05-05 13:40:04 +0100

    clerk webhook

--- types/index.ts
+++ types/index.ts
@@ -1,16 +1,10 @@
 // ====== USER PARAMS
 export type CreateUserParams = {
   clerkId: string
-  firstName: string
-  lastName: string
-  username: string
   email: string
   photo: string
 }
 
 export type UpdateUserParams = {
-  firstName: string
-  lastName: string
-  username: string
   photo: string
 }
